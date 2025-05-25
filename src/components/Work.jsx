08e1195302f51578c79151f2a2326bc82113d16c
anurodh pancholi <pancholianurodh@gmail.com>
2025-05-25 13:07:55 +0530

    changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@ const Work = () => {
     <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">Welcome to my web development portfolio! Here you'll find a curated selection of projects that showcase my skills in both frontend and full-stack development. From responsive user interfaces to robust backend integrations, each project highlights my passion for building seamless and scalable web applications.</p>
 
     <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
-        <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+        {/* <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
 
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
@@ -22,13 +22,15 @@ const Work = () => {
                     <img src={send_icon} alt="" className="w-5"/>
                 </div>
             </div>
-        </div>
+        </div> */}
+
+
         <div className="aspect-square bg-[url('./assets/work-3.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
 
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
-                    <h2 className="font-semibold">WEB APPLICATION</h2>
-                    <p className="text-sm text-gray-700">ML/LLM-Integrated Tool</p>
+                    <h2 className="font-semibold">Bolg Generator</h2>
+                    <p className="text-sm text-gray-700">Web Application using ML/LLM-Integrated Tool</p>
                 </div>
                 <a
       href="https://short-blog-writer.onrender.com"  
@@ -40,19 +42,25 @@ const Work = () => {
     </a>
             </div>
         </div>
+
+
+
         <div className="aspect-square bg-[url('./assets/work-3.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
 
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
-                    <h2 className="font-semibold">Photography site</h2>
-                    <p className="text-sm text-gray-700">Web Design</p>
+                    <h2 className="font-semibold">CareConnect : A Appointment Management System</h2>
+                    <p className="text-sm text-gray-700">MERN Stack Application using MongoDB, Express.js, React.js, and Node.js</p>
                 </div>
                 <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
                     <img src={send_icon} alt="" className="w-5"/>
                 </div>
             </div>
         </div>
-        <div className="aspect-square bg-[url('./assets/work-4.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+
+
+
+        {/* <div className="aspect-square bg-[url('./assets/work-4.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
 
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
@@ -63,7 +71,7 @@ const Work = () => {
                     <img src={send_icon} alt="" className="w-5"/>
                 </div>
             </div>
-        </div>
+        </div> */}
     </div>
     <a href="https://github.com/anupancholi" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500
     dark:text-white dark:border-white dark:hover:bg-darkHover">Show more
